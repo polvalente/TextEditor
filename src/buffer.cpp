c5bffb2dc8fd878c033a700f9e2b0fab2852d032
Paulo Valente <polvalente@gmail.com>
2017-06-01 19:58:05 -0300

    Comentando buffer.cpp

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -6,15 +6,16 @@ using std::string;
 using std::vector;
 
 Buffer::Buffer(){
-	lines = new vector<string>(0);
+	lines = new vector<string>(0); // Alocar vector vazio para linhas do buffer. Alocacao dinamica para evitar estouro de pilha
 }
 
 Buffer::~Buffer(){
-	delete lines;
+	delete lines; // apaga o vector alocado
 }
 
 
 string Buffer::clean(string line){
+	//Metodo que substitui tabs por 4 espacos na string recebida e retorna uma versao modificada dela
 	size_t tab;
 	while ((tab = line.find("\t")) != line.npos){
 		line.replace(tab, 1, "    ");
@@ -23,20 +24,24 @@ string Buffer::clean(string line){
 }
 
 void Buffer::insertLine(string line, int i){
+	//Metodo que insere a string 'line' na posicao i do vector de linhas (lines)
 	line = clean(line);
 	lines->insert(lines->begin()+i, line);
 }
 
 void Buffer::appendLine(string line){
+	//Metodo que insere a string 'line' no final do vector de linhas (lines)
 	line = clean(line);
 	lines->push_back(line);
 }
 
 void Buffer::removeLine(int i){
+	//Metodo que remove a string 'line' na posicao i do vector de linhas (lines)
 	if (i >= 0 && lines->size() >= 1)
 		lines->erase(lines->begin()+i);
 }
 
 void Buffer::deleteContent(){
+	//Metodo que apaga todo  conteudo do vector de linhas (lines)
 	lines->erase(lines->begin(), lines->end());
 }
